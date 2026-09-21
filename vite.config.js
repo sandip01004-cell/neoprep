0d6ef25f45f1f14efd1930e14440def260fbc9a5
sandip01004-cell <sandip01004@gmail.com>
2026-09-22 00:10:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/neoprep/', // 👈 ADD THIS (repo name)
+
   plugins: [
     react(),
     VitePWA({
